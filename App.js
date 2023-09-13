de234a248a4c5232e12faf488135e892fb7cda1e
Promish Khaniya <promishkhaniya@Promishs-Air.ht.home>
2023-09-12 23:49:56 -0400

    first lab

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>AboutMe</Text>
+      <Text>Promish Khaniya</Text>
+      <Text>ID#: 301369717</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
